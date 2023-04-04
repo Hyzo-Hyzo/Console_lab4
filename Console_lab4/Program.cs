c31f8d3eb8894dd859f1d50d217bca07293bce1e
Ira <mazuririnochkacat@gmail.com>
2023-04-04 20:21:11 +0300

    all project

--- Console_lab4/Program.cs
+++ Console_lab4/Program.cs
@@ -5,8 +5,9 @@
         static void Main(string[] args)
         {
             //Task1.num1();
-            Task2.num2();
+            //Task2.num2();
             //GuessNumberGame.Task3.num3();
+            PseudoTextGenerator.Task4.num4();
         }
     }
 }
